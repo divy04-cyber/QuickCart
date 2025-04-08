db1b9544504c95435317c5049b28c7b232dbc4ee
divy04-cyber <divyanshparihar04@gmail.com>
2025-04-08 14:56:24 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint:{
+        ignoreDuringBuilds:true
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com',
+                pathname: '**',
+            },
+        ],
+    },
+};
+
+export default nextConfig;
